1509dadb68b3942215399afe1631930b071dceb6
johnPaslawski <jpaslawski93@gmail.com>
2021-11-03 17:36:34 +0100

    Add project files.

---
+++ GameOfSpace/Controllers/Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameOfSpace.Controllers
+{
+    //[Authorize]
+    //[ApiController]
+    //[Route("[controller]")]
+    
+}
