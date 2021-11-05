c4f83d301f4e9e66c51bf7b8ee66e8a9ad48f1ab
johnPaslawski <jpaslawski93@gmail.com>
2021-11-05 23:29:27 +0100

    Component Game & WelcomeScreen added, useApiGet custom hook implemented, react-router-dom@5 installed

--- game_of_space_frontend/src/App.js
+++ game_of_space_frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import WelcomeScreen from './WelcomeScreen';
+import WelcomeScreen from './WelcomeScreen/WelcomeScreen';
+import Game from './Game/Game';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <WelcomeScreen/>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ game_of_space_frontend/src/Game/Game.js
@@ -0,0 +1,11 @@
+const Game = () => {
+    
+    
+    return (
+        <div>
+            GAME
+        </div>
+    );
+}
+ 
+export default Game;
