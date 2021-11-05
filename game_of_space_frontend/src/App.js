c91a2ce904ef5c7b1e00ef78bcddcd6ab74b0ba9
johnPaslawski <jpaslawski93@gmail.com>
2021-11-05 15:41:17 +0100

    React App created, WelcomeScreen - Background, Title & Footer added

---
+++ game_of_space_frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WelcomeScreen from './WelcomeScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ game_of_space_frontend/src/WelcomeScreenFooter.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const WelcomeScreenFooter = () => {
+    return (
+        <div className="WelcomeScreenFooter">
+            © 2021 by <a className="LinkToGit" href="https://github.com/johnPaslawski">johnPaslawski.</a>  All rights reserved | Version: 1.0
+        </div>
+    );
+}
+
+export default WelcomeScreenFooter;
